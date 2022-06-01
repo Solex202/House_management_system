4629decad296ac178dea8af27106428902719977
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-06-01 14:31:28 +0100

    first commit

---
+++ src/main/java/com/semicolon/africa/House/Management/System/HouseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.House.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semicolon/africa/House/Management/System/HouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.africa.House.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
