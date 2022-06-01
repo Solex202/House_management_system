4f04d406c5e5745b072563dacbc9d2c6774d385c
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-06-01 22:45:45 +0100

    new commit

---
+++ src/main/java/com/semicolon/africa/House/Management/System/dtos/UserDto.java
@@ -0,0 +1,11 @@
+package com.semicolon.africa.House.Management.System.dtos;
+
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String message;
+    private String email;
+    private boolean loginStatus;
+}
---
+++ src/main/java/com/semicolon/africa/House/Management/System/dtos/request/CreateUserRequest.java
@@ -0,0 +1,32 @@
+package com.semicolon.africa.House.Management.System.dtos.request;
+
+import com.semicolon.africa.House.Management.System.data.models.Gender;
+import com.semicolon.africa.House.Management.System.data.models.Room;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Validated
+public class CreateUserRequest {
+
+    private String firstName;
+    private String lastName;
+    @Email
+    private String email;
+    @Min(6)
+    private String password;
+    @Min(6)
+    private String confirmPassword;
+
+    private Gender gender;
+    private Room room;
+}
