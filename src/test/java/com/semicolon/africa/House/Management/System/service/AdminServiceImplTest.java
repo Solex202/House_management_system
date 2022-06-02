d11e37f0eeedafea4ec3167b9cdfe97e96c491be
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-06-02 20:44:25 +0100

    new commit

---
+++ src/test/java/com/semicolon/africa/House/Management/System/service/AdminServiceImplTest.java
@@ -0,0 +1,202 @@
+package com.semicolon.africa.House.Management.System.service;
+
+import com.semicolon.africa.House.Management.System.data.models.Gender;
+import com.semicolon.africa.House.Management.System.data.models.Room;
+import com.semicolon.africa.House.Management.System.data.models.RoomType;
+import com.semicolon.africa.House.Management.System.data.models.User;
+import com.semicolon.africa.House.Management.System.dtos.request.AssignRoomRequest;
+import com.semicolon.africa.House.Management.System.dtos.request.BookRoomRequest;
+import com.semicolon.africa.House.Management.System.dtos.response.AssignRoomResponse;
+import com.semicolon.africa.House.Management.System.exception.RoomNumberDoesNotExistException;
+import com.semicolon.africa.House.Management.System.exception.UserNotFoundException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DataMongoTest
+class AdminServiceImplTest {
+    @Autowired
+    private AdminService adminService;
+
+
+    @Autowired
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testThatAdminUserCamAssignRoom(){
+        BookRoomRequest bookRoomRequest = BookRoomRequest.builder()
+                .firstName("lota")
+                .lastName("solomon")
+                .email("lota@gmail.com")
+                .password("lota123")
+                .confirmPassword("lota123")
+                .gender(Gender.MALE)
+                .build();
+
+        userService.bookRoom(bookRoomRequest);
+
+        BookRoomRequest bookRoomRequest2 = BookRoomRequest.builder()
+                .firstName("gina")
+                .lastName("dimma")
+                .email("gina@gmail.com")
+                .password("ginagina")
+                .confirmPassword("ginagina")
+                .gender(Gender.FEMALE)
+                .build();
+
+        userService.bookRoom(bookRoomRequest2);
+
+        List<User> users = userService.getAllUsers();
+        assertThat(users.size(), equalTo(2));
+
+        Room room = new Room();
+        room.setRoomNumber(3);
+        room.setRoomType(RoomType.FEMALE_WING);
+        AssignRoomRequest assignRoomRequest = new AssignRoomRequest(room, bookRoomRequest2.getEmail());
+
+
+        AssignRoomResponse assignRoomResponse = adminService.assignRoom(assignRoomRequest);
+        assertThat(assignRoomResponse.getMessage(), is("room has been assigned"));
+
+    }
+
+    @Test
+    void testThatAdminUserCannotAssignRoomWhenRoomNumberIsExceeded_throwException(){
+        BookRoomRequest bookRoomRequest = BookRoomRequest.builder()
+                .firstName("lota")
+                .lastName("solomon")
+                .email("lota@gmail.com")
+                .password("lota123")
+                .confirmPassword("lota123")
+                .gender(Gender.MALE)
+                .build();
+
+        userService.bookRoom(bookRoomRequest);
+
+        BookRoomRequest bookRoomRequest2 = BookRoomRequest.builder()
+                .firstName("gina")
+                .lastName("dimma")
+                .email("gina@gmail.com")
+                .password("ginagina")
+                .confirmPassword("ginagina")
+                .gender(Gender.FEMALE)
+                .build();
+
+        userService.bookRoom(bookRoomRequest2);
+
+        List<User> users = userService.getAllUsers();
+        assertThat(users.size(), equalTo(2));
+
+        Room room = new Room();
+        room.setRoomNumber(62);
+        room.setRoomType(RoomType.FEMALE_WING);
+        AssignRoomRequest assignRoomRequest = new AssignRoomRequest(room, bookRoomRequest2.getEmail());
+
+        assertThrows(RoomNumberDoesNotExistException.class,()-> adminService.assignRoom(assignRoomRequest));
+
+    }
+
+    @Test
+    void testThatAdminUserCannotAssignRoomWhenRoomNumberIsBelow(){
+        BookRoomRequest bookRoomRequest = BookRoomRequest.builder()
+                .firstName("lota")
+                .lastName("solomon")
+                .email("lota@gmail.com")
+                .password("lota123")
+                .confirmPassword("lota123")
+                .gender(Gender.MALE)
+                .build();
+
+        userService.bookRoom(bookRoomRequest);
+
+        BookRoomRequest bookRoomRequest2 = BookRoomRequest.builder()
+                .firstName("gina")
+                .lastName("dimma")
+                .email("gina@gmail.com")
+                .password("ginagina")
+                .confirmPassword("ginagina")
+                .gender(Gender.FEMALE)
+                .build();
+
+        userService.bookRoom(bookRoomRequest2);
+
+        List<User> users = userService.getAllUsers();
+        assertThat(users.size(), equalTo(2));
+
+        Room room = new Room();
+        room.setRoomNumber(-2);
+        room.setRoomType(RoomType.FEMALE_WING);
+        AssignRoomRequest assignRoomRequest = new AssignRoomRequest(room, bookRoomRequest2.getEmail());
+
+        assertThrows(RoomNumberDoesNotExistException.class,()-> adminService.assignRoom(assignRoomRequest));
+
+    }
+
+    @Test
+    void testThatAdminUserCannotAssignRoomWhenUserIsNotInDatabase_throwException(){
+        Room room = new Room();
+        room.setRoomNumber(7);
+        room.setRoomType(RoomType.FEMALE_WING);
+        AssignRoomRequest assignRoomRequest = new AssignRoomRequest(room, "chioma@gmail.com");
+
+        assertThrows(UserNotFoundException.class,()-> adminService.assignRoom(assignRoomRequest));
+
+    }
+
+    @Test
+    void testThatAdminUserCanEvictOccupant(){
+        BookRoomRequest bookRoomRequest = BookRoomRequest.builder()
+                .firstName("lota")
+                .lastName("solomon")
+                .email("lota@gmail.com")
+                .password("lota123")
+                .confirmPassword("lota123")
+                .gender(Gender.MALE)
+                .build();
+
+        userService.bookRoom(bookRoomRequest);
+
+        BookRoomRequest bookRoomRequest2 = BookRoomRequest.builder()
+                .firstName("gina")
+                .lastName("dimma")
+                .email("gina@gmail.com")
+                .password("ginagina")
+                .confirmPassword("ginagina")
+                .gender(Gender.FEMALE)
+                .build();
+
+        userService.bookRoom(bookRoomRequest2);
+
+        List<User> users = userService.getAllUsers();
+        assertThat(users.size(), equalTo(2));
+
+        Room room = new Room();
+        room.setRoomNumber(2);
+        room.setRoomType(RoomType.FEMALE_WING);
+        AssignRoomRequest assignRoomRequest = new AssignRoomRequest(room, bookRoomRequest2.getEmail());
+
+        AssignRoomResponse assignRoomResponse = adminService.assignRoom(assignRoomRequest);
+        assertThat(assignRoomResponse.getMessage(), is("room has been assigned"));
+
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        userService.deleteAll();
+    }
+}
