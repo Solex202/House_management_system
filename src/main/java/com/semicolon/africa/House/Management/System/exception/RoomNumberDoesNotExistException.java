1d7906e1644ac22870265e02d1f061e299be6dc0
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-06-02 14:17:40 +0100

    new commit

---
+++ src/main/java/com/semicolon/africa/House/Management/System/exception/RoomNumberDoesNotExistException.java
@@ -0,0 +1,8 @@
+package com.semicolon.africa.House.Management.System.exception;
+
+public class RoomNumberDoesNotExistException extends RuntimeException{
+
+    public RoomNumberDoesNotExistException(String message) {
+        super(message);
+    }
+}
