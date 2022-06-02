75389b708328669f335f3ab4e8f9703f1db608a1
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-06-02 20:46:03 +0100

    new commit

---
+++ src/main/java/com/semicolon/africa/House/Management/System/exception/MaleWingException.java
@@ -0,0 +1,8 @@
+package com.semicolon.africa.House.Management.System.exception;
+
+public class MaleWingException extends RuntimeException {
+    public MaleWingException(String message) {
+
+        super(message);
+    }
+}
