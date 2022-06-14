f79154c155f17866bb912029ef87e48e7c1a71a3
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-06-14 10:08:04 +0100

    change

---
+++ src/main/java/com/semicolon/africa/House/Management/System/exception/PaymentException.java
@@ -0,0 +1,7 @@
+package com.semicolon.africa.House.Management.System.exception;
+
+public class PaymentException extends RuntimeException {
+    public PaymentException(String message) {
+        super(message);
+    }
+}
