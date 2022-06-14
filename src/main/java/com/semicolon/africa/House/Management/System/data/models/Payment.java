6ab903d8513714a82a9d091ad5ed08d4103c4ae5
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-06-14 17:46:11 +0100

    change

---
+++ src/main/java/com/semicolon/africa/House/Management/System/data/models/Payment.java
@@ -0,0 +1,19 @@
+package com.semicolon.africa.House.Management.System.data.models;
+
+public enum Payment {
+
+    TWO_HUNDRED_THOUSAND(200_000),
+    THREE_HUNDRED_THOUSAND(400_000),
+    SIX_HUNDRED_THOUSAND(600_000)
+    ;
+
+    private final int value;
+
+    Payment(int value) {
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
