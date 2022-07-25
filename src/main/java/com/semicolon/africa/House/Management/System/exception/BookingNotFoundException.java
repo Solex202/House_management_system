3e73cab2bdffa63a77b065ee260117fe165e4060
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-07-25 13:51:05 +0100

    update

---
+++ src/main/java/com/semicolon/africa/House/Management/System/exception/BookingNotFoundException.java
@@ -0,0 +1,8 @@
+package com.semicolon.africa.House.Management.System.exception;
+
+public class BookingNotFoundException extends RuntimeException{
+
+    public BookingNotFoundException(String message) {
+        super(message);
+    }
+}
